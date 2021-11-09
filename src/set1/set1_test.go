e85aeea4f8dbf77c5ea8ca98621f7c7cd9e3a4f1
Jonathan Lam <jlam55555@gmail.com>
2021-11-09 05:08:14 -0500

    added exercise 8

--- src/set1/set1_test.go
+++ src/set1/set1_test.go
@@ -107,3 +107,27 @@ func TestChallenge7(t *testing.T) {
 		t.Errorf("test case failed: got %v, expected %v", res, out)
 	}
 }
+
+func TestChallenge8(t *testing.T) {
+	path := "../../res/8.txt"
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	res, maxScore := "", -1
+	for scanner.Scan() {
+		if score := AesEcbDetect(scanner.Text()); score > maxScore {
+			maxScore, res = score, scanner.Text()
+		}
+	}
+
+	out := "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a"
+
+	if res != out {
+		t.Errorf("test case failed: got %v, expected %v", res, out)
+	}
+}
